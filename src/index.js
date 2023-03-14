d269fc865f2d08a082e0fb1f3d1687afb845832b
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-03-14 19:54:09 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+// const talker = require('./talker.json');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +12,19 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  const filePath = path.join(__dirname, 'talker.json'); 
+  const data = fs.readFileSync(filePath);
+  const talker = JSON.parse(data);
+  console.log(talker);
+
+  if (talker) {
+    res.status(HTTP_OK_STATUS).json(talker);
+  } else {
+    res.status(HTTP_OK_STATUS).json([]);
+  }
 });
 
 app.listen(PORT, () => {
