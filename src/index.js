fd50849cf20a921d2ab5946cfc3165a78dea8191
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2023-03-14 21:44:51 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const talker = require('./talker.json');
 const fs = require('fs');
 const path = require('path');
 
@@ -7,7 +6,11 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
+const NOT_FOUND_MESAGE = {
+  message: 'Pessoa palestrante não encontrada',
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -18,12 +21,26 @@ app.get('/talker', (req, res) => {
   const filePath = path.join(__dirname, 'talker.json'); 
   const data = fs.readFileSync(filePath);
   const talker = JSON.parse(data);
-  console.log(talker);
 
   if (talker) {
     res.status(HTTP_OK_STATUS).json(talker);
   } else {
     res.status(HTTP_OK_STATUS).json([]);
+  }
+});
+
+app.get('/talker/:id', (req, res) => {
+  const filePath = path.join(__dirname, 'talker.json'); 
+  const data = fs.readFileSync(filePath);
+  const talkers = JSON.parse(data);
+  const { id } = req.params;
+
+  const talkerId = talkers.find((talker) => Number(id) === talker.id);
+
+  if (talkerId) {
+    res.status(HTTP_OK_STATUS).json(talkerId);
+  } else {
+    res.status(HTTP_NOT_FOUND_STATUS).json(NOT_FOUND_MESAGE);
   }
 });
 
